141420e1ce3ebd5003d0b9c0620d3c5dea2cdeee
Georgi Kamacharov <gkamacharov@gmail.com>
2021-09-20 13:51:18 -0400

    add extension methods

---
+++ kamafi.core.data/ExtensionMethods.EntityFramework.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace kamafi.core.data
+{
+    public static partial class ExtensionMethods
+    {
+        public static PropertyBuilder HasSnakeCaseColumnName(this PropertyBuilder propertyBuilder)
+        {
+            propertyBuilder.Metadata.SetColumnName(
+                propertyBuilder
+                    .Metadata
+                    .Name
+                    .ToSnakeCase());
+
+            return propertyBuilder;
+        }
+    }
+}
---
+++ kamafi.core.data/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace kamafi.core.data
+{
+    public static partial class ExtensionMethods
+    {
+        public static string ToSnakeCase(this string str)
+        {
+            return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+        }
+    }
+}
