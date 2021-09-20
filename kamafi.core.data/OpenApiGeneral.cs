e28735b7b7e1159ae840056dac5afb8a51776e5e
Georgi Kamacharov <gkamacharov@gmail.com>
2021-09-20 16:27:31 -0400

    update to add OpenApiGeneral class

---
+++ kamafi.core.data/OpenApiGeneral.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kamafi.core.data
+{
+    public class OpenApiGeneral
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+    }
+}
--- kamafi.core.middleware/OpenApi.cs
+++ kamafi.core.middleware/OpenApi.cs
@@ -5,26 +5,25 @@ using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
+using kamafi.core.data;
+
 namespace kamafi.core.middleware
 {
     public static partial class MiddlewareExtensions
     {
         public static IServiceCollection AddKamafiSwaggerGen(
             this IServiceCollection services,
-            string name,
-            string title,
-            string version,
-            string description,
+            OpenApiGeneral general,
             OpenApiContact contact,
             OpenApiLicense license)
         {
             services.AddSwaggerGen(x =>
             {
-                x.SwaggerDoc(name, new OpenApiInfo
+                x.SwaggerDoc(general.Name, new OpenApiInfo
                 {
-                    Title = title,
-                    Version = version,
-                    Description = description,
+                    Title = general.Title,
+                    Version = general.Version,
+                    Description = general.Description,
                     Contact = contact,
                     License = license
                 });
