7461f89a17e98335725814f27be34fa856d840df
Georgi Kamacharov <gkamacharov@gmail.com>
2021-09-22 11:49:09 -0400

    update to add services and event repo

---
+++ kamafi.core.data/Event.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kamafi.core.data
+{
+    public class Event
+    {
+        public string EventType { get; set; }
+        public EventSource Source { get; set; } = new EventSource();
+        public EventUser User { get; set; } = new EventUser();
+        public EventEntity Entity { get; set; } = new EventEntity();
+    }
+
+    public class EventSource
+    {
+        public string Api { get; }
+        public string Ip { get; set; }
+    }
+
+    public class EventUser
+    {
+        public int? Id { get; set; }
+        public Guid? PublicKey { get; set; }
+    }
+
+    public class EventEntity
+    {
+        public int? Id { get; set; }
+        public string Type { get; set; }
+        public object Payload { get; set; }
+    }
+}
---
+++ kamafi.core.services/EventRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+using RestSharp;
+using Polly;
+
+using kamafi.core.data;
+
+namespace kamafi.core.services
+{
+    public class EventRepository : IEventRepository
+    {
+        private readonly ILogger<EventRepository> _logger;
+        private readonly ITenant _tenant;
+        private readonly IRestClient _client;
+
+        public EventRepository(
+            ILogger<EventRepository> logger,
+            ITenant tenant,
+            IRestClient client)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _tenant = tenant ?? throw new ArgumentNullException(nameof(tenant));
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task EmitAsync<T>(Event ev)
+            where T : Event, new()
+        {
+            try
+            {
+                await Policy.Handle<HttpRequestException>()
+                    .OrResult<IRestResponse>(x => (int)x.StatusCode == StatusCodes.Status500InternalServerError)
+                    .RetryAsync(3)
+                    .ExecuteAsync(async () =>
+                    {
+                        var request = new RestRequest("/emit", Method.POST).AddJsonBody(ev);
+
+                        return await _client.ExecuteAsync(request);
+                    });
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("{Tenant} | Error while emitting event={EventName}. Message={EventErrorMessage}",
+                    _tenant.Log,
+                    nameof(T),
+                    e.Message);
+            }
+        }
+    }
+}
